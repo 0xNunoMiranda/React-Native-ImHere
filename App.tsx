1bb0189c0204c291fb73f4fb3cd1ecb3849c206c
Nuno Miranda <nuno_miranda@outlook.com>
2024-05-13 20:41:58 +0100

    update

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View}from'react-native';
+export default function App(){
+  return(
+    <View style={{
+      flex:1,
+      backgroundColor:'#131016',
+      padding:24
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    }}>
+
+      <Text style={{color:'#FDFCFE'}}>Ola mundo</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
